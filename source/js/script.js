23b943e7e14f7282bd1533c4639ce12198eb84a8
Kitkat <hinhilla@list.ru>
2021-09-20 18:25:36 +0300

    создание слайдера и его анимация

--- source/js/script.js
+++ source/js/script.js
@@ -6,3 +6,40 @@ const toToggleModalByButton = (body) => body.classList.toggle('show-modal');
 buttonNavigation.addEventListener('click', () => toToggleModalByButton(body));
 
 
+const sliderButtonRight = document.querySelector('.slider__button-right');
+
+let slideCurrent;
+let slideNotCurrent;
+
+sliderButtonRight.addEventListener('click', () => {
+  slideCurrent = document.querySelector('.slider__slide--current');
+  slideNotCurrent = document.querySelector('.slider__slide--not-current');
+  slideCurrent.classList.remove('slider__slide--current');
+  slideCurrent.classList.remove('animation--current');
+  slideCurrent.classList.remove('animation--current-left');
+  slideCurrent.classList.add('slider__slide--not-current');
+  slideCurrent.classList.add('animation--not-current');
+  slideNotCurrent.classList.remove('slider__slide--not-current');
+  slideNotCurrent.classList.add('slider__slide--current');
+  slideNotCurrent.classList.remove('animation--not-current');
+  slideNotCurrent.classList.remove('animation--not-current-left');
+  slideNotCurrent.classList.add('animation--current');
+
+})
+
+const sliderButtonLeft = document.querySelector('.slider__button-left');
+
+sliderButtonLeft.addEventListener('click', () => {
+  slideCurrent = document.querySelector('.slider__slide--current');
+  slideNotCurrent = document.querySelector('.slider__slide--not-current');
+  slideCurrent.classList.remove('slider__slide--current');
+  slideCurrent.classList.remove('animation--current');
+  slideCurrent.classList.remove('animation--current-left');
+  slideCurrent.classList.add('slider__slide--not-current');
+  slideCurrent.classList.add('animation--not-current-left');
+  slideNotCurrent.classList.remove('slider__slide--not-current');
+  slideNotCurrent.classList.add('slider__slide--current');
+  slideNotCurrent.classList.remove('animation--not-current');
+  slideNotCurrent.classList.remove('animation--not-current-left');
+  slideNotCurrent.classList.add('animation--current-left');
+})
